3394bbc936597ad998e02ff0501b78d290d3db0a
hesam80 <hm.hoseyni@gmail.com>
2020-03-24 22:20:26 +0430

    modify openfile

--- openfile.py
+++ openfile.py
@@ -1,14 +1,13 @@
 #!C:\Users\Pars\AppData\Local\Programs\Python\Python38-32\python.exe
-import hello
-import stdio
+
 print("Content-Type: text/html\n charset:utf-8\n")
 def hello(request):
     return HttpResponse("Hello world")
     print("content-type: text/html\n\n" )
 
 
-f= open('matris.txt')
-e = open('tst.php')
+
+e = open('tost.php')
 content = e.read()
 print(content)
 print("<br><B>hello python on github</B>")
