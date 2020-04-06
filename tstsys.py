bbe39ce13ec460f00243e4f9f97085314dea6c17
hesam80 <hm.hoseyni@gmail.com>
2020-04-06 19:39:29 +0430

    not changes anymore

--- tstsys.py
+++ tstsys.py
@@ -1,8 +1,14 @@
+#!C:\Users\Pars\AppData\Local\Programs\Python\Python38-32\python.exe
 import sys
 import urllib.request
+import datetime
 print(sys.platform)
-page = urllib.request.urlopen('http://hesam80.pergig.ir')
-print(page)
-for line in page :
-	print(line)
-	
+#page = urllib.request.urlopen('http://hesam80.pergig.ir')
+#print(page)
+#for line in page :
+	#print(line)
+
+no = datetime.datetime.now()
+print(no)
+
+
